55676c4dce2e5b2986730be2db4952f03fb462a2
Sergiu Grigoras <sergiu.grigoras@yahoo.com>
2023-10-02 18:09:23 -0400

    Update @angular/core and @angular/cli to version 16.

--- ClientApp/src/app/services/auth-guard.service.ts
+++ ClientApp/src/app/services/auth-guard.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot } from '@angular/router';
 import { environment } from 'src/environments/environment';
 import { AuthService } from './auth.service';
 
 const apiUrl: string = environment.baseUrl;
 
 @Injectable()
-export class AuthGuard implements CanActivate {
+export class AuthGuard  {
     constructor(private authService: AuthService, private router: Router) {
     }
 
--- ClientApp/src/app/services/reg-guard.service.ts
+++ ClientApp/src/app/services/reg-guard.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class RegGuard implements CanActivate {
+export class RegGuard  {
 
   constructor(private authService: AuthService, private router: Router) { }
 
