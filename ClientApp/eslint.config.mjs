9beb71560baca4ff45ab5172aaf26a988f87a033
Sergiu Grigoras <seriojag@gmail.com>
2025-10-12 11:04:29 -0400

    Migrate eslint config.

---
+++ ClientApp/eslint.config.mjs
@@ -0,0 +1,40 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default defineConfig([globalIgnores(["projects/**/*"]), {
+    files: ["**/*.ts"],
+
+    extends: compat.extends(
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@angular-eslint/recommended",
+        "prettier",
+    ),
+
+    languageOptions: {
+        ecmaVersion: 5,
+        sourceType: "script",
+
+        parserOptions: {
+            project: ["tsconfig.json"],
+            createDefaultProgram: true,
+        },
+    },
+
+    rules: {},
+}, {
+    files: ["**/*.html"],
+    extends: compat.extends("plugin:@angular-eslint/template/recommended", "prettier"),
+    rules: {},
+}]);
